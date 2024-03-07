82c15d510e0b79863ad7903100d48664938761ea
FathurRozzaq <fathurrozzaq99@gmail.com>
2024-03-07 17:08:58 +0700

    Mengatur timestamps model Data menjadi true

--- app/Models/Data.php
+++ app/Models/Data.php
@@ -10,5 +10,5 @@ class Data extends Model
     use HasFactory;
 
     protected $fillable = ['judul', 'nama_ketua', 'nim_ketua', 'jurusan_ketua', 'univ_ketua', 'alamat_ketua', 'hp_ketua', 'email_ketua', 'jml_anggota', 'dosbing', 'nidn', 'alamat_dosen', 'hp_dosen', 'dikti', 'sumber_lain', 'waktu_kerja', 'kota', 'tanggal', 'latbel'];
-    public $timestamps = false;
+    public $timestamps = true;
 }
